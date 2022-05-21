ab0dd9ebb32e667d7ff6e4a8701e7728cb7a07d8
Arsal Fadilah <88387951+arsalf@users.noreply.github.com>
2022-05-21 11:06:35 +0700

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from numpy import sign
 from signlanguage import SignLanguage
 import os
 
-# sign = SignLanguage(os.path.join('MP_Data'), ['hello', 'thanks', 'iloveu'], 15, 30)
-# sign.realtime_trainer()
-# sign.realtime_test('hello-thanks-iloveu.h5')
-# print(sign.file_name)
+sign = SignLanguage(os.path.join('MP_Data'), ['hello', 'thanks', 'iloveu'], 15, 30)
+sign.realtime_trainer()
+sign.realtime_test('hello-thanks-iloveu.h5')
+print(sign.file_name)
